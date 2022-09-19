b8d21ddb15cc67919d8a205dc79fa431192ae725
tianwailaikecxh <1064536454@qq.com>
2022-09-19 22:58:53 +0800

    first commit

---
+++ src/leetcode/Problem496.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Problem496 {
+    public static void main(String[] args){
+
+    }
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int[] result = new int[len1];
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer,Integer> map= new HashMap<>();
+        for(int i = 0;i<len2;i++){
+            while(!stack.empty() && nums2[i]>stack.peek()){
+                map.put(stack.pop(),nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+        while(!stack.isEmpty()){
+            map.put(stack.pop(),-1);
+        }
+        for(int i = 0;i<len1;i++){
+            result[i] = map.get(nums1[i]);
+        }
+        return result;
+    }
+}
---
+++ src/leetcode/Problem503.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class Problem503 {
+    public static void main(String[] args){
+        System.out.println(0);
+    }
+}
